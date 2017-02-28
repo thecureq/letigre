a1720d43bdf85d86bb6460eb73265ad4fa3c31bf
thecureq <soursugar00@gmail.com>
2017-03-01 01:48:40 +0300

    done!

---
+++ javascripts/text.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".element").typed({
+    strings: ["FEMINISM\nAND RIOT GRRRRRRRRRRRRRRR\nRRRRRL"],
+    typeSpeed: 120,
+    loop: 999
+  });
+});
+
+$(document).ready(function(){
+  $(".element_2").typed({
+    strings: ["TICKEEEEEEEEEEEEEEEEEEEEEEEEEEEEET"],
+    typeSpeed: 120,
+    loop: 999
+  });
+});
